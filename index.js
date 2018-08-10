66289fcabdaf02d9ebb7f4556bcb8a10f7992a99
danielabdelsamed <daniel@abdelsamed.com>
2018-08-10 00:24:05 -0400

    Fixed up the codebase

--- index.js
+++ index.js
@@ -1,110 +1,91 @@
-var _ = require("lodash");
+var _ = require("lodash")
 
-var request = require("express/lib/request");
+module.exports = function (app) {
+    // Add middleware to app to add req.runMiddleware
+    app.use((req, res, next) => {
+        req.runMiddleware = (path, options, callback) => {
+            if (_.isFunction(options)) {
+                callback = options
+                options = {}
+            }
+            options.original_req = req
+            options.original_res = res
+            app.runMiddleware(path, options, callback)
+        }
+        next()
+    })
 
-module.exports = function(app) {
-  app.use((req, res, next) => {
-    req.runMiddleware = (path, options, callback) => {
-      if (_.isFunction(options)) {
-        callback = options;
-        options = {};
-      }
-      options.original_req = req;
-      options.original_res = res;
-      app.runMiddleware(path, options, callback);
-    };
-    next();
-  });
-  if (app.runMiddleware) return; // Do not able to add us twice
+    // Check to make sure app.runMiddleware hasn't already been added
+    if (app.runMiddleware) return
 
-  app.runMiddleware = function(path, options, callback) {
-    if (callback) callback = _.once(callback);
-    if (typeof options === "function") {
-      callback = options;
-      options = null;
+    // Add runMiddleware to the app object
+    app.runMiddleware = function (path, options, callback) {
+        if (callback) callback = _.once(callback)
+        if (typeof options === "function") {
+            callback = options
+            options = null
+        }
+        options = options || {}
+        options.url = path
+        let new_req, new_res
+        if (options.original_req) {
+            new_req = options.original_req
+            for (let i in options) {
+                if (i === "original_req") continue
+                new_req[i] = options[i]
+            }
+        } else {
+            new_req = createReq(path, options)
+        }
+        new_res = createRes(callback)
+        app(new_req, new_res)
     }
-    options = options || {};
-    options.url = path;
-    var new_req, new_res;
-    if (options.original_req) {
-      new_req = options.original_req;
-      for (var i in options) {
-        if (i === "original_req") continue;
-        new_req[i] = options[i];
-      }
-    } else {
-      new_req = createReq(path, options);
-    }
-    new_res = createRes(callback);
-    app(new_req, new_res);
-  };
-
-  /* end - APP.runMiddleware*/
-};
-
-function createReq(path, options) {
-  if (!options) options = {};
-  var req = _.extend(
-    {
-      method: "GET",
-      host: "",
-      cookies: {},
-      query: {},
-      url: path,
-      headers: {},
-    },
-    options
-  );
-  req.method = req.method.toUpperCase();
-  // req.connection=_req.connection
-  return req;
 }
-function createRes(callback) {
-  var res = {
-    _removedHeader: {},
-  };
-  // res=_.extend(res,require('express/lib/response'));
 
-  var headers = {};
-  var code = 200;
-  res.set = res.header = (x, y) => {
-    if (arguments.length ==== 2) {
-      res.setHeader(x, y);
-    } else {
-      for (var key in x) {
-        res.setHeader(key, x[key]);
-      }
+/**
+ * Create synthetic request object
+ * @param {string} path
+ * @param {Object} options
+ * @returns {Object} request
+ */
+function createReq(path, options) {
+    if (!options) options = {}
+    const req = _.extend(
+        {
+            method: "GET",
+            host: "",
+            cookies: {},
+            query: {},
+            url: path,
+            headers: {},
+        },
+        options
+    )
+    req.method = req.method.toUpperCase()
+    return req
+}
+
+/**
+ * Create synthetic response object
+ * @param {Function} callback
+ * @returns {Object} response
+ */
+function createRes(callback) {
+    const res = {
+        _removedHeader: {},
     }
-    return res;
-  }
-  res.setHeader = (x, y) => {
-    headers[x] = y;
-    headers[x.toLowerCase()] = y;
-    return res;
-  };
-  // res.get=(x) => {
-  // 	return headers[x]
-  // }
-  res.redirect = function(_code, url) {
-    if (!_.isNumber(_code)) {
-      code = 301;
-      url = _code;
-    } else {
-      code = _code;
+    const headers = {}
+    let code = 200
+    res.set = res.header = (header, value) => {
+        headers[header] = value
+        return res
     }
-    res.setHeader("Location", url);
-    res.end();
-    // callback(code,url)
-  };
-  res.status = function(number) {
-    code = number;
-    return res;
-  };
-  res.end = res.send = res.write = function(data) {
-    if (callback) callback(code, data, headers);
-    // else if (!options.quiet){
-    //     _res.send(data)
-    // }
-  };
-  return res;
+    res.status = function (number) {
+        code = number
+        return res
+    }
+    res.end = res.send = res.write = function (data) {
+        if (callback) callback(code, data, headers)
+    }
+    return res
 }
